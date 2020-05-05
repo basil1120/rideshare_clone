3fdef506cec5f927a02a5a6ff82b056d992cd49b
Jeremiah Aura [Digital Banking] <jaura@CO-OPBANK.CO.KE>
2020-05-05 18:22:29 +0300

    first commit

---
+++ app/src/main/java/com/rideshare/okoarides/PayPalConfig.java
@@ -0,0 +1,7 @@
+package com.rideshare.okoarides;
+
+class PayPalConfig {
+
+    public static final String PAYPAL_CLIENT_ID = "AfFYSj-izBJIf2JJ99un9QFgDuZzDsiB7sRHKFl7je-TzrciK2BNgHCkapEg-eD7hoSsHPKKL4ZjJVJP";
+    public static final String PAYPAL_CLIENT_SECRET = "EFZVbg_Y5xzPOTN8-evqanhh8kaBL3DzCrQNvIaO4yikS55ohtirFzoMtxBpg6ZwOEmZa69oT2Ma9T4e";
+}
